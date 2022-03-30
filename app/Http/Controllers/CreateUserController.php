554de7af6a5b6f0620bc5d7080acfb16a8f5fb33
BRYAN INC <bryanlopez2kul2@yahoo.com>
2022-03-30 01:29:25 +0100

    Refactor CreateUserController

--- app/Http/Controllers/CreateUserController.php
+++ app/Http/Controllers/CreateUserController.php
@@ -5,15 +5,20 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateUserRequest;
-use App\Http\Resources\CreatedUserResource;
+use App\Http\Resources\AccesssTokenResource;
 use App\Services\CreateUserService;
+use Illuminate\Http\Response;
 use Laravel\Passport\Http\Controllers\AccessTokenController;
 use Psr\Http\Message\ServerRequestInterface;
 
 final class CreateUserController extends AccessTokenController
 {
-    public function __invoke(CreateUserRequest $request, CreateUserService $service, ServerRequestInterface $serverRequest): CreatedUserResource
+    public function __invoke(CreateUserRequest $request, CreateUserService $service, ServerRequestInterface $serverRequest): AccesssTokenResource
     {
-        return new CreatedUserResource($service->FromRequest($request), $this->issueToken($serverRequest)->content());
+        return new AccesssTokenResource(
+            $service->FromRequest($request),
+            $this->issueToken($serverRequest)->content(),
+            Response::HTTP_CREATED
+        );
     }
 }

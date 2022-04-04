9ff25a00d7c32ec9583ee22017a2a6eeecba9082
Mac <bryanlopez2kul2@yahoo.com>
2022-04-04 16:16:16 +0100

    Add refresh token route

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+use Laravel\Passport\RouteRegistrar;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,5 +25,14 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        if (!$this->app->routesAreCached()) {
+            Passport::routes(function (RouteRegistrar $router) {
+                $router->forTransientTokens();
+            });
+        }
+
+        Passport::tokensExpireIn(now()->addMinutes(30));
+        Passport::refreshTokensExpireIn(now()->addDay());
     }
 }

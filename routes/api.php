c01a3d4871a94f6cda85e6e55f618d3597835cd1
Mac <bryanlopez2kul2@yahoo.com>
2022-04-18 22:58:26 +0100

    refactor routes file

--- routes/api.php
+++ routes/api.php
@@ -1,63 +1,45 @@
 <?php
 
 use App\Http\Controllers;
-use App\Http\Middleware as MW;
+use App\Http\Middleware\ConvertNestedValuesToArrayMiddleware as ConvertStringToArray;
+use App\Http\Middleware\HandleDbTransactionsMiddleware as DBTransaction;
 use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
 
 Route::middleware('auth:api')->group(function () {
 
     Route::get('bookmarks', Controllers\FetchUserBookmarksController::class)->name('fetchUserBookmarks');
-    Route::get('favourites', Controllers\FetchUserFavouritesController::class)->name('fetchUserFavourites');
-    Route::get('user/sites', Controllers\FetchUserSitesController::class)->name('fetchUserSites');
+    Route::get('users/favourites', Controllers\FetchUserFavouritesController::class)->name('fetchUserFavourites');
+    Route::get('users/sites', Controllers\FetchUserSitesController::class)->name('fetchUserSites');
 
-    Route::post('bookmarks', Controllers\CreateBookmarkController::class)
-        ->middleware([MW\ConvertNestedValuesToArrayMiddleware::keys('tags'), Mw\HandleDbTransactionsMiddleware::class])
-        ->name('createBookmark');
+    Route::middleware(DBTransaction::class)->group(function () {
+        Route::post('bookmarks', Controllers\CreateBookmarkController::class)
+            ->middleware([ConvertStringToArray::keys('tags')])
+            ->name('createBookmark');
 
-    Route::post('users', Controllers\CreateUserController::class)
-        ->middleware([Mw\HandleDbTransactionsMiddleware::class])
-        ->withoutMiddleware('auth:api')
-        ->name('createUser');
+        Route::delete('bookmarks', Controllers\DeleteBookmarkController::class)->name('deleteBookmark');
+        Route::delete('bookmarks/site', Controllers\DeleteBookmarksFromSiteController::class)->name('deleteBookmarksFromSite');
+        Route::post('favourites', Controllers\CreateFavouriteController::class)->name('createFavourite');
+        Route::delete('favourites', Controllers\DeleteFavouriteController::class)->name('deleteFavourite');
 
-    Route::post('users/password/request-token', Controllers\RequestPasswordResetTokenController::class)
-        ->middleware([Mw\HandleDbTransactionsMiddleware::class, Laravel\Passport\Http\Middleware\CheckClientCredentials::class])
-        ->withoutMiddleware('auth:api')
-        ->name('requestPasswordResetToken');
+        Route::delete('bookmarks/tags/remove', Controllers\DeleteBookmarkTagsController::class)
+            ->middleware([ConvertStringToArray::keys('tags')])
+            ->name('deleteBookmarkTags');
 
-    Route::post('users/password/reset', Controllers\ResetPasswordController::class)
-        ->middleware([Mw\HandleDbTransactionsMiddleware::class, Laravel\Passport\Http\Middleware\CheckClientCredentials::class])
-        ->withoutMiddleware('auth:api')
-        ->name('resetPassword');
-
-    Route::post('login', Controllers\LoginController::class)
-        ->withoutMiddleware('auth:api')
-        ->name('loginUser');
-
-    Route::delete('bookmarks', Controllers\DeleteBookmarkController::class)
-        ->middleware([Mw\HandleDbTransactionsMiddleware::class])
-        ->name('deleteBookmark');
-
-    Route::delete('bookmarks/site', Controllers\DeleteBookmarksFromSiteController::class)
-        ->middleware([Mw\HandleDbTransactionsMiddleware::class])
-        ->name('deleteBookmarksFromSite');
-
-    Route::delete('bookmarks/tags/remove', Controllers\DeleteBookmarkTagsController::class)
-        ->middleware([Mw\HandleDbTransactionsMiddleware::class, MW\ConvertNestedValuesToArrayMiddleware::keys('tags')])
-        ->name('deleteBookmarkTags');
-
-    Route::patch('bookmarks', Controllers\UpdateBookmarkController::class)
-        ->middleware([Mw\HandleDbTransactionsMiddleware::class, MW\ConvertNestedValuesToArrayMiddleware::keys('tags')])
-        ->name('updateBookmark');
-
-    Route::post('favourites', Controllers\CreateFavouriteController::class)
-        ->middleware([Mw\HandleDbTransactionsMiddleware::class])
-        ->name('createFavourite');
-
-    Route::delete('favourites', Controllers\DeleteFavouriteController::class)
-        ->middleware([Mw\HandleDbTransactionsMiddleware::class])
-        ->name('deleteFavourite');
+        Route::patch('bookmarks', Controllers\UpdateBookmarkController::class)
+            ->middleware([ConvertStringToArray::keys('tags')])
+            ->name('updateBookmark');
+    });
 });
 
-Route::post('client/oauth/token', Controllers\IssueClientTokenController::class)
-    ->withoutMiddleware('auth:api')
-    ->name('issueClientToken');
+Route::post('client/oauth/token', Controllers\IssueClientTokenController::class)->name('issueClientToken');
+Route::post('users', Controllers\CreateUserController::class)->middleware([DBTransaction::class])->name('createUser');
+Route::post('login', Controllers\LoginController::class)->name('loginUser');
+
+Route::post('users/password/request-token', Controllers\RequestPasswordResetTokenController::class)
+    ->middleware([DBTransaction::class, CheckClientCredentials::class])
+    ->name('requestPasswordResetToken');
+
+Route::post('users/password/reset', Controllers\ResetPasswordController::class)
+    ->middleware([DBTransaction::class, CheckClientCredentials::class])
+    ->name('resetPassword');

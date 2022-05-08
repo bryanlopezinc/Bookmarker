34f859a4e720d3b5dea2601955d1f6c25293571a
Mac <bryanlopez2kul2@yahoo.com>
2022-05-08 13:34:13 +0100

    refactor BookmarkResource

--- app/Http/Resources/BookmarkResource.php
+++ app/Http/Resources/BookmarkResource.php
@@ -19,17 +19,17 @@ final class BookmarkResource extends JsonResource
         return [
             'type' => 'bookmark',
             'attributes'      => [
-                'id'                 => $this->bookmark->id->toInt(),
-                'title'              => $this->bookmark->title->value,
-                'web_page_link'      => $this->bookmark->linkToWebPage->value,
+                'id' => $this->bookmark->id->toInt(),
+                'title' => $this->bookmark->title->value,
+                'web_page_link' => $this->bookmark->linkToWebPage->value,
                 'has_preview_image'  => $this->bookmark->hasPreviewImageUrl,
                 'preview_image_url'  => $this->when($this->bookmark->hasPreviewImageUrl, fn () => $this->bookmark->previewImageUrl->value),
-                'description'        => $this->when(!$this->bookmark->description->isEmpty(), fn () => $this->bookmark->description->value),
-                'has_description'    => !$this->bookmark->description->isEmpty(),
-                'site_id'            => $this->bookmark->webPagesiteId->toInt(),
-                'bookmarked_on'      => $this->bookmark->timeCreated->timeStamp->diffForHumans(),
-                'from_site'          => new WebSiteResource($this->bookmark->fromWebSite),
-                'tags'               => $this->bookmark->tags->toStringCollection()->all()
+                'description' => $this->when(!$this->bookmark->description->isEmpty(), fn () => $this->bookmark->description->value),
+                'has_description'  => !$this->bookmark->description->isEmpty(),
+                'site_id' => $this->bookmark->webPagesiteId->toInt(),
+                'bookmarked_on' => $this->bookmark->timeCreated->timeStamp->diffForHumans(),
+                'from_site' => new WebSiteResource($this->bookmark->fromWebSite),
+                'tags' => $this->bookmark->tags->toStringCollection()->all()
             ]
         ];
     }

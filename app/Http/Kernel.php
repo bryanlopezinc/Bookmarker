e46f3dcf85e58cedfee5968920194f14a5835602
Mac <bryanlopez2kul2@yahoo.com>
2022-06-13 17:27:41 +0100

    Rename convertStringtoarrayMiddlware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'convertNestedValues' => \App\Http\Middleware\ConvertNestedValuesToArrayMiddleware::class,
+        'convertNestedValues' => \App\Http\Middleware\ConvertConcatenatedValuesToArrayMiddleware::class,
     ];
 }

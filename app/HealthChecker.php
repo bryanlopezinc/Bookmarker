ab8b6166d476fbd0fcd8a8c280bc026073736503
Mac <bryanlopez2kul2@yahoo.com>
2022-07-04 03:59:00 +0100

    refactor

--- app/HealthChecker.php
+++ app/HealthChecker.php
@@ -10,6 +10,7 @@ use App\DataTransferObjects\Bookmark;
 use App\ValueObjects\ResourceID;
 use Illuminate\Http\Client\Pool;
 use Illuminate\Http\Client\Response;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 
 final class HealthChecker
@@ -24,27 +25,19 @@ final class HealthChecker
             return;
         }
 
-        $healthCheckResults = [];
-
-        $responses = $this->getResponse(
+        $responses = $this->makeRequests(
             $bookmarks->filterByIDs($this->repository->whereNotRecentlyChecked($bookmarks->ids()))
         );
 
-        foreach ($responses as $id => $response) {
-            $healthCheckResults[] = new HealthCheckResult(new ResourceID($id), $response);
-        }
-
-        if (empty($healthCheckResults)) {
-            return;
-        }
-
-        $this->repository->update($healthCheckResults);
+        collect($responses)
+            ->map(fn (Response $response, int $bookmarkID) => new HealthCheckResult(new ResourceID($bookmarkID), $response))
+            ->whenNotEmpty(fn (Collection $collection) => $this->repository->update($collection->all()));
     }
 
     /**
      * @return array<int,Response> Each key in the array is the bookmarkID and value the corresponding http response.
      */
-    private function getResponse(BookmarksCollection $bookmarks): array
+    private function makeRequests(BookmarksCollection $bookmarks): array
     {
         return Http::pool(function (Pool $pool) use ($bookmarks) {
             return collect($bookmarks)->map(function (Bookmark $bookmark) use ($pool) {

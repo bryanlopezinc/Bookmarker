c25fdb34625b298ea775417046ca645818d2a68e
Mac <bryanlopez2kul2@yahoo.com>
2022-07-15 23:48:49 +0100

    refactor Providers

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,12 +2,17 @@
 
 namespace App\Providers;
 
+use App\Contracts\BookmarksHealthRepositoryInterface;
 use App\Contracts\CreateBookmarkRepositoryInterface;
 use App\Contracts\UpdateBookmarkRepositoryInterface;
+use App\HealthChecker;
+use App\Repositories\BookmarksHealthRepository;
 use App\Repositories\CreateBookmarkRepository;
 use App\Repositories\UpdateBookmarkRepository;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Application;
+use Tests\TestBookmarksHealthRepository;
 
 class RepositoryServiceProvider extends ServiceProvider implements DeferrableProvider
 {
@@ -20,13 +25,31 @@ class RepositoryServiceProvider extends ServiceProvider implements DeferrablePro
     {
         $this->app->bind(CreateBookmarkRepositoryInterface::class, fn () => app(CreateBookmarkRepository::class));
         $this->app->bind(UpdateBookmarkRepositoryInterface::class, fn () => app(UpdateBookmarkRepository::class));
+
+        $this->bindBookmarksHealth();
+    }
+
+    private function bindBookmarksHealth(): void
+    {
+        $concrete = new BookmarksHealthRepository;
+
+        $this->app->bind(BookmarksHealthRepositoryInterface::class, fn () => $concrete);
+
+        //Prevent faking Http response for every route that checks bookmarks health during tests.
+        $this->app->addContextualBinding(
+            HealthChecker::class,
+            BookmarksHealthRepositoryInterface::class,
+            fn (Application $app) => $app->environment('testing') ? new TestBookmarksHealthRepository($concrete) : $concrete
+        );
     }
 
     public function provides()
     {
         return [
             CreateBookmarkRepositoryInterface::class,
-            UpdateBookmarkRepositoryInterface::class
+            UpdateBookmarkRepositoryInterface::class,
+            BookmarksHealthRepositoryInterface::class,
+            HealthChecker::class
         ];
     }
 }
--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\BookmarksHealthRepositoryServiceProvider::class,
 
         App\IpGeoLocation\ServiceProvider::class,
         App\DeviceDetector\ServiceProvider::class,

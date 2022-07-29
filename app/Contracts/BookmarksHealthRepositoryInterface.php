0972fd808e207e28793a1f41af206a13dabd8baa
Mac <bryanlopez2kul2@yahoo.com>
2022-07-29 13:10:58 +0100

    Refactor BookmarksHealthRepositoryInterface

--- app/Contracts/BookmarksHealthRepositoryInterface.php
+++ app/Contracts/BookmarksHealthRepositoryInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Contracts;
 
-use App\Collections\ResourceIDsCollection;
+use App\Collections\ResourceIDsCollection as Collection;
 use App\HealthCheckResult;
 
 interface BookmarksHealthRepositoryInterface
@@ -13,7 +13,7 @@ interface BookmarksHealthRepositoryInterface
      * Get the bookmark IDs that has not been recently checked
      * with the ids that has never been checked from the given bookmark IDs.
      */
-    public function whereNotRecentlyChecked(ResourceIDsCollection $bookmarkIDs): ResourceIDsCollection;
+    public function whereNotRecentlyChecked(Collection $bookmarkIDs): Collection;
 
     /**
      * Update the given bookmarks health status.

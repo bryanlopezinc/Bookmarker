bb3673d88c34bb24e165bb3834215bfbe42f63f6
Mac <bryanlopez2kul2@yahoo.com>
2022-09-29 01:11:20 +0100

    stop validation on failure - createUser

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -16,10 +16,10 @@ final class CreateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'username' => Username::rules(['required', Rule::unique(User::class, 'username')]),
+            'username' => Username::rules(['bail', 'required', Rule::unique(User::class, 'username')]),
             'firstname'  => ['required', 'filled', join(':', ['max', setting('FIRSTNAME_MAX_LENGTH')])],
             'lastname'  => ['required', 'filled', join(':', ['max', setting('LASTNAME_MAX_LENGTH')])],
-            'email' => ['required', 'email', Rule::unique(User::class, 'email'), Rule::unique(SecondaryEmail::class, 'email')],
+            'email' => ['bail', 'required', 'email', Rule::unique(User::class, 'email'), Rule::unique(SecondaryEmail::class, 'email')],
             'password' => ['required', 'confirmed', Password::defaults()],
         ];
     }

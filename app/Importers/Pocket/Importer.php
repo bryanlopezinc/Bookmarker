271bfb2330bf3f0a5242b9fb44cad8030282555e
Mac <bryanlopez2kul2@yahoo.com>
2022-10-20 22:07:44 +0100

    Refactor pocketImporter

--- app/Importers/Pocket/Importer.php
+++ app/Importers/Pocket/Importer.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace App\Importers\Pocket;
 
-use App\Exceptions\InvalidTagException;
 use App\Exceptions\MalformedURLException;
 use App\Importers\ResolveImportTimestamp;
 use App\Importers\Filesystem;
 use App\Importers\ImporterInterface;
+use App\Importers\ResolveBookmarkTags;
 use App\Services\CreateBookmarkService;
-use App\ValueObjects\Tag;
 use App\ValueObjects\Url;
 use App\ValueObjects\UserID;
 use App\ValueObjects\Uuid;
@@ -18,7 +17,7 @@ use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
 final class Importer implements ImporterInterface
 {
-    use ResolveImportTimestamp;
+    use ResolveImportTimestamp, ResolveBookmarkTags;
 
     public function __construct(
         private CreateBookmarkService $createBookmark,
@@ -40,52 +39,19 @@ final class Importer implements ImporterInterface
         $this->filesystem->delete($userID, $requestID);
     }
 
-    private function saveBookmark(array $requestData, UserID $userID, Bookmark $pocketBookmark): void
+    private function saveBookmark(array $requestData, UserID $userID, Bookmark $bookmark): void
     {
         try {
-            $url = new Url($pocketBookmark->url);
+            $url = new Url($bookmark->url);
         } catch (MalformedURLException) {
             return;
         }
 
         $this->createBookmark->fromArray([
             'url' => $url,
-            'createdOn' => $this->resolveImportTimestamp($requestData['use_timestamp'] ?? true, $pocketBookmark->timestamp),
+            'createdOn' => $this->resolveImportTimestamp($requestData['use_timestamp'] ?? true, $bookmark->timestamp),
             'userID' => $userID,
-            'tags' => $this->resolveTags($requestData, $pocketBookmark)
+            'tags' => $this->resolveTags($bookmark->tags, $requestData['ignore_tags'] ?? false)
         ]);
-    }
-
-    private function resolveTags(array $requestData, Bookmark $pocketBookmark): array
-    {
-        $tags = [];
-
-        if ($requestData['ignore_tags'] ?? false) {
-            return $tags;
-        }
-
-        if (count($pocketBookmark->tags) > setting('MAX_BOOKMARKS_TAGS')) {
-            return $tags;
-        }
-
-        foreach ($pocketBookmark->tags as $tag) {
-            if (!$this->tagIsCompatible($tag)) {
-                continue;
-            }
-
-            $tags[] = $tag;
-        }
-
-        return collect($tags)->uniqueStrict()->values()->all();
-    }
-
-    private function tagIsCompatible(string $tag): bool
-    {
-        try {
-            new Tag($tag);
-            return true;
-        } catch (InvalidTagException) {
-            return false;
-        }
     }
 }

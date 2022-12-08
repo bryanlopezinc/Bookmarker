d1eead726b92217f66852b63495e914377c773d2
Mac <bryanlopez2kul2@yahoo.com>
2022-12-08 16:09:36 +0100

    filter collaborators by permissions

---
+++ app/Http/Requests/FetchFolderCollaboratorsRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use App\PaginationData;
+use App\Rules\ResourceIdRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
+
+final class FetchFolderCollaboratorsRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'folder_id' => ['required', new ResourceIdRule],
+            'permissions' => ['sometimes', 'array', 'in:view_only,addBookmarks,removeBookmarks,inviteUser,updateFolder'],
+            'permissions.*' => ['distinct:strict', 'filled'],
+            ...PaginationData::new()->asValidationRules()
+        ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function (Validator $validator) {
+            if (filled($validator->failed())) {
+                return;
+            }
+
+            $filter = collect($this->input('permissions', []));
+
+            $filter->when($filter->contains('view_only') && $filter->count() > 1, function () use ($validator) {
+                $validator->errors()->add('permissions', 'Cannot request collaborator with only view permissions with any other permission');
+            });
+        });
+    }
+}
